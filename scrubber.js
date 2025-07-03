bf1203b3bbf78311b3a21ff32ed72a98d99b88b3
Xavier Poon <xavier@habitedge.app>
2025-07-03 21:52:36 +1000

    better animations and add progress track

--- scrubber.js
+++ scrubber.js
@@ -12,7 +12,6 @@ ScrubberView.prototype.makeAccessors = function () {
   var min = 0;
   var max = 1;
   var step = 0;
-  var orientation = 'horizontal';
 
   this.value = function (_value) {
     if (_value === undefined) return value;
@@ -21,13 +20,13 @@ ScrubberView.prototype.makeAccessors = function () {
     _value = Math.max(min, Math.min(max, _value));
 
     if (step > 0) {
-      var nsteps = Math.round((_value - min)/step);
+      var nsteps = Math.round((_value - min) / step);
 
-      var invStep = 1/step;
+      var invStep = 1 / step;
       if (invStep === Math.round(invStep)) {
-        _value = (min*invStep + nsteps)/invStep;
+        _value = (min * invStep + nsteps) / invStep;
       } else {
-        _value = (min/step + nsteps)*step;
+        _value = (min / step + nsteps) * step;
       }
 
       value = Math.max(min, Math.min(max, _value));
@@ -63,58 +62,45 @@ ScrubberView.prototype.makeAccessors = function () {
     this.redraw();
     return this;
   };
-
-  this.orientation = function(_orientation) {
-    if (_orientation === undefined) return orientation;
-    if (_orientation === orientation) return this;
-    orientation = _orientation;
-    this.redraw();
-    return this;
-  };
 };
 
 ScrubberView.prototype.createDOM = function () {
-  this.elt = document.createElement('div');
-  this.track = document.createElement('div');
-  this.thumb = document.createElement('div');
+  this.elt = document.createElement("div");
+  this.track = document.createElement("div");
+  this.progress = document.createElement("div");
+  this.thumb = document.createElement("div");
 
-  this.elt.className = this.orientation() === 'horizontal' ? 'scrubber' : 'scrubber-vert';
-  this.track.className = 'track';
-  this.thumb.className = 'thumb';
+  this.elt.className = "scrubber";
+  this.track.className = "track";
+  this.progress.className = "progress";
+  this.thumb.className = "thumb";
 
   this.elt.appendChild(this.track);
+  this.elt.appendChild(this.progress);
   this.elt.appendChild(this.thumb);
 };
 
 ScrubberView.prototype.redraw = function () {
-  var frac = (this.value() - this.min())/(this.max() - this.min());
-  if (this.orientation() === 'horizontal') {
-    this.elt.className = 'scrubber';
-    this.thumb.style.top = '50%';
-    this.thumb.style.left = frac*100 + '%';
-  }
-  else {
-    this.elt.className = 'scrubber-vert';
-    this.thumb.style.left = '50%';
-    this.thumb.style.top = 100 - (frac*100) + '%';
-  }
+  var frac = (this.value() - this.min()) / (this.max() - this.min());
+  this.elt.className = "scrubber";
+  this.thumb.style.top = "50%";
+  this.thumb.style.left = frac * 100 + "%";
+  this.progress.style.width = frac * 100 + "%";
 };
 
-ScrubberView.prototype.attachListeners = function ()  {
+ScrubberView.prototype.attachListeners = function () {
   var self = this;
   var mousedown = false;
   var cachedLeft;
   var cachedWidth;
-  var cachedTop;
-  var cachedHeight;
 
   var start = function (evt) {
     evt.preventDefault();
     self.onScrubStart(self.value());
-    
-    document.addEventListener('mouseup', stop);
-    document.addEventListener('touchend', stop);
-    
+
+    document.addEventListener("mouseup", stop);
+    document.addEventListener("touchend", stop);
+
     mousedown = true;
     var rect = self.elt.getBoundingClientRect();
     // NOTE: page[X|Y]Offset and the width and height
@@ -127,69 +113,48 @@ ScrubberView.prototype.attachListeners = function ()  {
 
     cachedLeft = rect.left + xOffset;
     cachedWidth = rect.width;
-    cachedTop = rect.top + yOffset;
-    cachedHeight = rect.height;
-    self.thumb.className +=  ' dragging';
+    self.thumb.className += " dragging";
   };
 
   var stop = function () {
-    document.removeEventListener('mouseup', stop);
-    document.removeEventListener('touchend', stop);
+    document.removeEventListener("mouseup", stop);
+    document.removeEventListener("touchend", stop);
     mousedown = false;
     cachedLeft = undefined;
     cachedWidth = undefined;
-    cachedTop = undefined;
-    cachedHeight = undefined;
-    self.thumb.className = 'thumb';
+    self.thumb.className = "thumb";
     self.onScrubEnd(self.value());
   };
 
   var setValueFromPageX = function (pageX) {
-    var frac = Math.min(1, Math.max((pageX - cachedLeft)/cachedWidth, 0));
-    self.value((1-frac)*self.min() + frac*self.max());
+    var frac = Math.min(1, Math.max((pageX - cachedLeft) / cachedWidth, 0));
+    self.value((1 - frac) * self.min() + frac * self.max());
   };
 
-  var setValueFromPageY = function (pageY) {
-    var frac = Math.min(1, Math.max(1 - (pageY - cachedTop)/cachedHeight, 0));
-    self.value((1-frac)*self.min() + frac*self.max());
-  };
+  this.elt.addEventListener("mousedown", start);
+  this.elt.addEventListener("touchstart", start);
 
-  this.elt.addEventListener('mousedown', start);
-  this.elt.addEventListener('touchstart', start);
-
-  document.addEventListener('mousemove', function (evt) {
+  document.addEventListener("mousemove", function (evt) {
     if (!mousedown) return;
     evt.preventDefault();
-    if (self.orientation() === 'horizontal')
-      setValueFromPageX(evt.pageX);
-    else
-      setValueFromPageY(evt.pageY);
+    setValueFromPageX(evt.pageX);
   });
 
-  document.addEventListener('touchmove', function (evt) {
+  document.addEventListener("touchmove", function (evt) {
     if (!mousedown) return;
     evt.preventDefault();
-    if (self.orientation() === 'horizontal')
-      setValueFromPageX(evt.changedTouches[0].pageX);
-    else
-      setValueFromPageY(evt.changedTouches[0].pageY);
+    setValueFromPageX(evt.changedTouches[0].pageX);
   });
 
-  this.elt.addEventListener('mouseup', function (evt) {
+  this.elt.addEventListener("mouseup", function (evt) {
     if (!mousedown) return;
     evt.preventDefault();
-    if (self.orientation() === 'horizontal')
-      setValueFromPageX(evt.pageX);
-    else
-      setValueFromPageY(evt.pageY);
+    setValueFromPageX(evt.pageX);
   });
 
-  this.elt.addEventListener('touchend', function (evt) {
+  this.elt.addEventListener("touchend", function (evt) {
     if (!mousedown) return;
     evt.preventDefault();
-    if (self.orientation() === 'horizontal')
-      setValueFromPageX(evt.changedTouches[0].pageX);
-    else
-      setValueFromPageY(evt.changedTouches[0].pageY);
+    setValueFromPageX(evt.changedTouches[0].pageX);
   });
 };
